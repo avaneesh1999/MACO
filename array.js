9fcc38c74c7c41b7fd79302525af0378f40de410
Avaneesh <avaneeshsingh1999@gmail.com>
2021-12-16 16:39:16 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+// PROBLEM
+
+let arr=[1,2,3];
+// let arr1=arr;
+// arr1.push(4);
+// console.log(arr);
+// console.log(arr1);
+
+//SOLUTION
+
+let arr1=[...arr];
+arr1.push(4);
+console.log(arr);
+console.log(arr1);
