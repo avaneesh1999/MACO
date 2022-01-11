74eaf422f111eda784ad38d388831ef547a2017d
Avaneesh <avaneeshsingh1999@gmail.com>
2022-01-11 13:20:27 +0530

    movies-loader

---
+++ REACT/movies/src/Components/Banner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Banner extends Component {
+    render() {
+        return (
+            <div class="card" >
+  <img class="card-img-top" src="..." alt="Card image cap"/>
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+        )
+    }
+}
---
+++ REACT/movies/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div style={{color:'blue',display:'flex'}}>
+                <h1>Movies</h1>
+                <h2>Favourites</h2>
+                
+            </div>
+        )
+    }
+}
